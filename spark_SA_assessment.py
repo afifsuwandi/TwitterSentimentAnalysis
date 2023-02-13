fcb29c8a48bcf490c855d65c44ced3f6d692fb06
Muhammad Afif Mohamad Suwandi <125153586+afifsuwandi@users.noreply.github.com>
2023-02-13 17:03:29 +0800

    Update spark_SA_assessment.py

--- spark_SA_assessment.py
+++ spark_SA_assessment.py
@@ -32,19 +32,57 @@ def remove_features(data_str):
     return data_str
 
    
-  
+ 
+def polarity(value):
+    
+    if value > 0:
+        return "+ve"
+    elif value < 0:
+        return "-ve"
+    else:
+        return "neu"
+
+
+#.filter(lambda x:len(x)==8)\
    
 #Write your main function here
-def main()
+def main(sc, filename):
+    
+    data = sc.textFile(filename)\
+           .map(lambda x:x.split(','))\
+           .filter(lambda x:len(x)==8)\
+           .filter(lambda x:len(x[0])>1)\
+           .map(lambda x:x[4])\
+           .map(lambda x:abb_en(x))\
+           .map(lambda x:remove_features(x))\
+           .map(lambda x:TextBlob(x).sentiment.polarity)\
+           .map(lambda x:polarity(x))
+            
+    data_raw = sc.textFile(filename)\
+               .map(lambda x:x.split(','))\
+               .filter(lambda x:len(x)==8)\
+               .filter(lambda x:len(x[0])>1)\
+               .map(lambda x:(x[4],x[0],x[2],x[1],x[3],x[5],x[6],x[7]))
+      
+    
+    data_zip = data.zip(data_raw).map(lambda x:str(x).replace("'","").replace('"',""))
+    
+    #print (data.take(3))
+    #print (data_raw.take(4))
+    #print (data_zip.take(4))
    
    
 
-  
+    data_zip.saveAsTextFile("DE22C04_Afif")
    
 
 if __name__ == "__main__":
-
+    
+    conf = SparkConf().setMaster("local[1]").setAppName("DE22C04_Test_Afif")
+    sc = SparkContext (conf=conf)
+    
+    filename = "starbuck_v1.csv"
   
-   main()
+    main(sc, filename)
 
-   sc.stop()
+    sc.stop()
